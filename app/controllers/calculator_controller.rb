3931b6bfc30640606e772c976525252604c0b1b9
Emily Boucher <emi.boucher@gmail.com>
2017-07-17 16:26:44 -0400

    commit

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,3 +1,15 @@
+require "typhoeus"
+
+
 class CalculatorController < ApplicationController
+
+  headers = {
+    Authorization:"Token token=\"#{ENV['GM_KEY']}\""
+
+    res = Typhoeus.get("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=GM_KEY", followlocation: true)
+      map_body = JSON.parse(res.body)
+
+
+
+
 end
- 
