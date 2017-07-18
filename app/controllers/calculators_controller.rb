bd64ccf0e26fb9efa2e80871ffc61de8b4f3e1f1
Emily Boucher <emi.boucher@gmail.com>
2017-07-18 14:49:57 -0400

    index view working in rails :D! comment added in controller (index) to create separate layout for embedded map. TODO(EB): move JS into js file, separate index.html.erb code into layout file

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,13 +1,7 @@
 class CalculatorsController < ApplicationController
 
   def index
-    
-    res = Typhoeus.get("https://maps.googleapis.com/maps/api/js?key=#{ENV['GM_KEY']}&libraries=places", followlocation: true)
-      body = JSON.parse(res.body)
 
-      render json: {
-        origin: body["routes"][0]['legs'][0]['start_address'],
-        destination: body["routes"][0]['legs'][0]['end_address']
-      }
+    # render separate layout for map page
   end
 end
