ba973081ff012876813f565fa3c6865da54330de
Emily Boucher <emi.boucher@gmail.com>
2017-07-18 22:26:48 -0400

    pseudocoding in separate map.js file to work out rules

--- separate_working_files/map.js
+++ separate_working_files/map.js
@@ -1,13 +1,21 @@
-1. build an object array of distances(sample)
+// 1. build an object array of distances(sample for sample code only, give in m as in maps)
 
-2. declare distance variable
-- -  need to call distance.valueOf();
+distanceInput = 5010
 
-3. choosing mode of transit:
-switch (cases) - walking, driving, transit
-  ##if transit; ask whether subway, streetcar, bus, train in dropdown (can break it down by legs at a future point)
-  ##if driving, ask what type of car in dropdown
+// 2. declare distance variable
+// - -  can call distance.valueOf(); OR set variable in js where called in text.
 
-4. setup metrics in switch
 
-5. return resutl
+// 3. choosing mode of transit:
+// switch (cases) - walking, driving, transit
+  // if transit; ask whether subway, streetcar, bus, train in dropdown (can break it down by legs at a future point)
+
+  
+
+  // if driving, ask what type of car in dropdown
+
+// 4. setup metrics
+
+// 5. calculated score
+
+// 6. return result + scores
