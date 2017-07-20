d3ef24ea994df906ab150fd41eebd2cbed214bf5
DocPrior <Sara Prior>
2017-07-20 15:34:50 -0400

    finish basic team learderboard

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,17 +1,20 @@
 class TeamsController < ApplicationController
   def index
+    @teams = Team.all.order(name: :desc)
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
   end
 
   def show
   end
 
-  def teams_params
-
+  def team_params
+    {name: params[:team][:name]}
   end
 end
