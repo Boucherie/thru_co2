29a68f04a70fd828b0f864b5cdffbe17d7655251
Emily Boucher <emi.boucher@gmail.com>
2017-07-26 13:58:35 -0400

    adding css for .graph class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     # score
     # add data to strong params, pass user_id from view with AJAX (render on page or pass w params from JS)
     if xhr?
-      redirect_to root_path
+      redirect_to users_path
     else
       redirect_back_or_to @score
     end
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
 
   def index
-  
+
 
   end
 
