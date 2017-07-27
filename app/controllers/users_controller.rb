5fe2bd15c808cc704a036e558dbef06c2221f50a
DocPrior <Sara Prior>
2017-07-27 10:55:16 -0400

    trying to fix score submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,16 +30,17 @@ class UsersController < ApplicationController
   def update
     # @user = User.find(params[:user_id])
     # @score = @user.scores.find(params[:id])
-    # byebug
     # if @score.save(add_trip)
     #   flash[:notice] = "Score Added!"
+    # end
+    # byebug
     # distanceInKilometers
     # score
     # add data to strong params, pass user_id from view with AJAX (render on page or pass w params from JS)
     if xhr?
       redirect_to users_path
-    else
-      redirect_back_or_to @score
+    # else
+    #   redirect_back_or_to @score
     end
 
   end
