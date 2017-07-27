7b398edfcd5797821fe22293c32ea8c8af8c7095
Emily Boucher <emi.boucher@gmail.com>
2017-07-27 14:26:29 -0400

    event listener not working. Approaching this from new angle.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    # @user = User.find(params[:user_id])
-    # @score = @user.scores.find(params[:id])
+    @user = User.find(params[:user_id])
+    @score = @user.scores.find(params[:id])
+    @score.save(add_trip)
     # byebug
     # if @score.save(add_trip)
     #   flash[:notice] = "Score Added!"
