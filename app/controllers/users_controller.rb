c4b541c035699abf67e7abf71c1a4abd6efdbaad
Emily Boucher <emi.boucher@gmail.com>
2017-07-28 11:31:27 -0400

    created input elements inside form, values to update on calculation and be packaged to send via button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,17 +40,13 @@ class UsersController < ApplicationController
     # score
     # add data to strong params, pass user_id from view with AJAX (render on page or pass w params from JS)
     if xhr?
-<<<<<<< HEAD
       redirect_to users_url(@user)
     else
       redirect_back_or_to @score
-=======
       redirect_to users_path
     # else
     #   redirect_back_or_to @score
->>>>>>> 0dca9b8769edea0439be1c2ab4ab794c48d4ddbb
     end
-
   end
 
   def index
