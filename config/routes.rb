622b51dd4b45f8c058161d941affe73dd2685c3b
DocPrior <Sara Prior>
2017-08-01 16:37:35 -0400

    add link for team leaderboard fix minor bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   # resources :sessions
   resources :calculators
   resources :teams, only: %i(new create index show)
+  get 'teams/show' => 'teams#show'
 
   # resource :users, only: %i(new create)
   # post 'users' => 'users#create'
-  # get 'users/new' => 'users#new'
+  get 'users/new' => 'users#new'
   # post 'users/update' => 'users#update'
-  # get 'users/show' => 'users#show'
+  get 'users/show' => 'users#show'
 
   # resource :sessions, only: %i(new create destroy)
   post 'sessions' => 'sessions#create'
